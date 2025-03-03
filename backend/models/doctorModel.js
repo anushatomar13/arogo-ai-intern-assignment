366a3d2e58a4e31659718fb41c2fcfef8995fa6c
Anusha Tomar <anushatoma913@gmail.com>
2025-03-03 18:55:09 +0530

    Added Doctor model with schema

---
+++ backend/models/doctorModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    specialization: { type: String, required: true },
+    experience: { type: Number, required: true },
+    phone: { type: String, required: true },
+    feesPerConsultation: { type: Number, required: true },
+    available: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Doctor", doctorSchema);
