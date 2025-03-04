04fe2aa67cdfa77305c5a6d29a93ae2616eca4df
Anusha Tomar <anushatoma913@gmail.com>
2025-03-04 22:57:45 +0530

    Added endpoint to fetch user's appointments

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -141,5 +141,18 @@ const cancelAppointment = async (req, res) => {
   }
 };
 
+//Get all appointments
+const getUserAppointments = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const appointments = await Appointment.find({ userId }).sort({ date: 1 });
 
-module.exports = { bookAppointment, cancelAppointment };
+    res.status(200).json({ appointments });
+  } catch (error) {
+    console.error("Error fetching user appointments:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+module.exports = { bookAppointment, cancelAppointment, getUserAppointments };
--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { bookAppointment,cancelAppointment } = require("../controllers/appointmentController");
+const { bookAppointment, cancelAppointment, getUserAppointments } = require("../controllers/appointmentController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/book", authMiddleware, bookAppointment);
 router.post("/cancel", authMiddleware, cancelAppointment); 
+router.get("/user", authMiddleware, getUserAppointments);
 
 module.exports = router;
